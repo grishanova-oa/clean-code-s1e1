663ca029ac7ba9ba5395c5b80a4239a65408f0c5
grishanova-oa <grishanova.oa@gmail.com>
2022-10-26 20:59:22 +0200

    refactor: reduce classname according to rule 3.3

--- app.js
+++ app.js
@@ -85,11 +85,11 @@ var editTask=function(){
     var editInput=listItem.querySelector('input[type=text]');
     var label=listItem.querySelector("label");
     var editBtn=listItem.querySelector(".block-task__btn-edit");
-    var containsClass=listItem.classList.contains("edit-mode");
-    //If class of the parent is .edit-mode
+    var containsClass=listItem.classList.contains("block-tasks__edit");
+    //If class of the parent is .block-tasks__edit
     if(containsClass){
 
-        //switch to .edit-mode
+        //switch to .block-tasks__edit
         //label becomes the inputs value.
         label.innerText=editInput.value;
         editBtn.innerText="Edit";
@@ -98,8 +98,8 @@ var editTask=function(){
         editBtn.innerText="Save";
     }
 
-    //toggle .edit-mode on the parent.
-    listItem.classList.toggle("edit-mode");
+    //toggle .block-tasks__edit on the parent.
+    listItem.classList.toggle("block-tasks__edit");
 };
 
 
